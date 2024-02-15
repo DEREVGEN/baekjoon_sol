5a14088f429d10630ac46a932157a551bedde334
DEREVGEN <96948245+DEREVGEN@users.noreply.github.com>
2024-02-15 16:27:24 +0900

    backjoon ~ 2024-02-15

---
+++ src/main/java/com/backjoon/solution/problem_11279/Main.java
@@ -0,0 +1,37 @@
+package com.backjoon.solution.problem_11279;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.PriorityQueue;
+import java.util.Queue;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(input.readLine());
+
+        Queue<Integer> pq = new PriorityQueue<>((o1, o2) -> o2-o1);
+
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < N; i++) {
+            int num = Integer.parseInt(input.readLine());
+
+            if (num == 0) {
+                if (pq.isEmpty())
+                    sb.append('0');
+                else
+                    sb.append(pq.poll());
+                sb.append('\n');
+
+            } else {
+                pq.offer(num);
+            }
+        }
+
+        System.out.println(sb);
+    }
+}
